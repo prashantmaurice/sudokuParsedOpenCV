e0f2ae9c9aad9163ddd0da0c20fc12678bd0f0aa
Prashant Maurice <sabertoothmaurice@gmail.com>
2015-08-16 02:45:03 +0530

    Added basic xcode project

---
+++ SudokuScanner/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  SudokuScanner
+//
+//  Created by Maurice Prashant on 16/08/15.
+//  Copyright (c) 2015 Maurice Prashant. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World! Maurice\n";
+    return 0;
+}
